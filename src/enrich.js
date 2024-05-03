ae01c8184a952af7fc5a47c0eaca7ef9a6f250ed
Jason Norwood-Young <jason@10layer.com>
2024-05-03 14:43:53 +0200

    Await for geodata

--- src/enrich.js
+++ src/enrich.js
@@ -3,10 +3,10 @@ import { parse_useragent } from './useragent.js';
 import { geolocate } from './geolocate.js';
 import { parse_utm } from './utm.js';
 
-export const enrich = function (hit) {
+export const enrich = async function (hit) {
     const referer = parse_referer(hit.referer);
     const useragent = parse_useragent(hit.ua);
-    const geo = geolocate(hit.ip);
+    const geo = await geolocate(hit.ip);
     const utm = parse_utm(hit.url);
     return { ...hit, ...referer, ...useragent, ...geo, ...utm };
 }
