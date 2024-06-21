266d45a50f3a91e17c0b1da09b5a766d19503580
Jason Norwood-Young <jason@10layer.com>
2024-06-21 10:04:36 +0000

    Use a referer lookup table if we don't find the referer in the request

--- src/enrich.js
+++ src/enrich.js
@@ -2,9 +2,13 @@ import { parse_referer } from './referer.js';
 import { parse_useragent } from './useragent.js';
 import { geolocate } from './geolocate.js';
 import { parse_utm } from './utm.js';
+import { referer_lookup } from './referer_lookup.js';
 
 export const enrich = async function (hit) {
-    const referer = parse_referer(hit.referer);
+    let referer = parse_referer(hit.referer);
+    if (!referer.referer_source) {
+        referer = referer_lookup(hit.url);
+    }
     const useragent = parse_useragent(hit.ua);
     const geo = await geolocate(hit.ip);
     const utm = parse_utm(hit.url);
---
+++ src/referer_lookup.js
@@ -0,0 +1,34 @@
+const referer_lookup_table = [
+    {
+        regex: /-n24\?/i,
+        referer_source: "https://news24.com"
+    },
+    {
+        regex: /-ewn\?/i,
+        referer_source: "https://www.ewn.co.za"
+    },
+    {
+        regex: /-dm\?/i,
+        referer_source: "https://www.dailymaverick.co.za"
+    },
+    {
+        regex: /-bd\?/i,
+        referer_source: "https://www.businesslive.co.za"
+    },
+];
+
+export const referer_lookup = function (url) {
+    let referer_medium = "direct";
+    let referer_source = "";
+    for (let i = 0; i < referer_lookup_table.length; i++) {
+        if (referer_lookup_table[i].regex.test(url)) {
+            referer_source = referer_lookup_table[i].referer_source;
+            referer_medium = "external";
+            break;
+        }
+    }
+    return {
+        referer_medium,
+        referer_source,
+    }
+}
